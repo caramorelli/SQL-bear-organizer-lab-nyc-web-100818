ad68222ec2621a93615592764b5cfa12eee4340f
caramorelli7@gmail.com <caramorelli7@gmail.com>
2018-10-16 14:42:33 -0400

    add files

---
+++ lib/insert.sql
@@ -0,0 +1,31 @@
+
+-- * Mr. Chocolate
+-- * Rowdy
+-- * Tabitha
+-- * Sergeant Brown
+-- * Melissa
+-- * Grinch
+-- * Wendy
+-- * unnamed (the bear that killed Tim didn't have a name; refer back to how to create a record that doesn't have one value)
+
+-- INSERT INTO
+-- bear (name, age, color, temperment, alive)
+-- VALUES
+-- (1, "Mr. Chocolate", "M", 35, "brown", ),
+-- (2, "Rowdy", 17, "grey"),
+-- (3, "Tabitha", 5, "grey", ),
+-- (4, "Sergeant Brown", 35, "black", ),
+-- (5, "Melissa", 17, "grey"),
+-- (6, "Grinch", 28, 'black'),
+-- (7, "Wendy", 17, "brown"),
+-- (8, null, ),
+
+INSERT INTO bears (id, name, age, gender, color, temperament, alive) VALUES
+(1,"Mr. Chocolate", 20, "M", "dark brown", "calm", 0),
+(2,"Rowdy", 10, "M", "black", "intense", 1),
+(3,"Tabitha", 6, "F", "dark brown", "Nice", 1),
+(4,"Sargeant Brown", 19, "M", "Green", "Slimy", 0),
+(5,"Melissa", 13, "F", "dark brown", "goofy", 1),
+(6,"Grinch", 2, "M", "Black", "Grinchy", 1),
+(7,"Wendy", 6, "F", "Blue", "naive", 1),
+(8,null, 20, "M", "black", "aggressive", 0);
