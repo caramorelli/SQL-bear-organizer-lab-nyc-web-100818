2a45aaae1ce60a80d465dc0c241d216a5a553823
caramorelli7@gmail.com <caramorelli7@gmail.com>
2018-10-16 14:48:11 -0400

    update file

--- lib/insert.sql
+++ lib/insert.sql
@@ -8,24 +8,14 @@
 -- * Wendy
 -- * unnamed (the bear that killed Tim didn't have a name; refer back to how to create a record that doesn't have one value)
 
--- INSERT INTO
--- bear (name, age, color, temperment, alive)
--- VALUES
--- (1, "Mr. Chocolate", "M", 35, "brown", ),
--- (2, "Rowdy", 17, "grey"),
--- (3, "Tabitha", 5, "grey", ),
--- (4, "Sergeant Brown", 35, "black", ),
--- (5, "Melissa", 17, "grey"),
--- (6, "Grinch", 28, 'black'),
--- (7, "Wendy", 17, "brown"),
--- (8, null, ),
-
-INSERT INTO bears (id, name, age, gender, color, temperament, alive) VALUES
-(1,"Mr. Chocolate", 20, "M", "dark brown", "calm", 0),
-(2,"Rowdy", 10, "M", "black", "intense", 1),
-(3,"Tabitha", 6, "F", "dark brown", "Nice", 1),
-(4,"Sargeant Brown", 19, "M", "Green", "Slimy", 0),
-(5,"Melissa", 13, "F", "dark brown", "goofy", 1),
-(6,"Grinch", 2, "M", "Black", "Grinchy", 1),
-(7,"Wendy", 6, "F", "Blue", "naive", 1),
-(8,null, 20, "M", "black", "aggressive", 0);
+INSERT INTO
+bears (id, name, age, gender, color, temperament, alive)
+VALUES
+(1, "Mr. Chocolate", 35, "M", "grey", "happy", 0),
+(2, "Rowdy", 17, "M", "brown", "cuddly", 1),
+(3, "Tabitha", 2, "F", "black", "friendly", 1),
+(4, "Sargeant Brown", 6, "M", "grey", "stiff", 0),
+(5, "Melissa", 9, "F", "black", "funny", 1),
+(6, "Grinch", 20, "M", "grey-brown", "mean", 1),
+(7, "Wendy", 26, "F", "grey-black", "wise", 1),
+(8, null, 19, "M", "grey", "tyrant", 0);
